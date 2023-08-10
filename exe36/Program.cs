95f3d4e25247bc74ed1738fc90ae9a90a8827e14
«Elena <leseee4ka@bk.ru>
2023-08-10 17:56:03 +0300

    exeу

---
+++ exe36/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+
+#region --- 00 ---
+Console.Clear();
+Console.Title = "Задача 36.";
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+var curConsoleColor = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(@"************************************************************************
+Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+************************************************************************");
+Console.ForegroundColor = curConsoleColor;
+#endregion
+
+#region --- 01. array ---
+
+int[] array = GetArray(10, 1, 100);
+Console.WriteLine(String.Join(" ", array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+int[] res = new int[size];
+for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+#endregion --- 01. array ---
+
+#region --- 02. operations ---
+int negativeSum = GetNegativeSum(array);
+
+int GetNegativeSum(int[] arr)
+{
+    int negativeSum = 0;
+    foreach (int el in arr)
+    {
+        if (el % 2 == 1) negativeSum += el;
+    }
+    return negativeSum;
+}
+Console.WriteLine(negativeSum);
+#endregion --- 02. operations ---
---
+++ exe38/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76 */
+
+#region --- 00 ---
+Console.Clear();
+Console.Title = "Задача 38.";
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+var curConsoleColor = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(@"************************************************************************
+Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным элементов массива.
+
+************************************************************************");
+Console.ForegroundColor = curConsoleColor;
+#endregion
+
+#region --- 01. array ---
+
+double[] array = new double[5];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.NextDouble() * 10;
+        Console.Write($"{array[i]:F2} ");
+    }
+
+#endregion --- 01. array ---
+
+#region --- 02. operations ---
+
+
+double minValue = array[0];
+double maxValue = array[0];
+int l = 0;
+while (l < array.Length)
+{
+    if (array[l] < minValue)
+        minValue = array[l];
+    if (array[l] > maxValue)
+        maxValue = array[l];
+        l++;
+}
+
+double ras = maxValue - minValue;
+
+
+Console.Write($" ->> Разница между максимальным и минимальным элементов массива: {ras:F2}");
+
+
+#endregion --- 02. operations ---
